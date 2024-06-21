8cdfe09a9621c3fdb4887374630dc08d8b0ae662
Ajaya Prajapati <ajz.prz@gmail.com>
2024-06-21 10:08:30 -0400

    ui changed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,72 +1,67 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAuthContext } from '../store/useContext';
-import TodoList from './TodoList';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../store/useContext";
+import TodoList from "./TodoList";
 import { IoIosAddCircleOutline } from "react-icons/io";
 
-
 const Home = () => {
-
   const navigate = useNavigate();
-  const {todoList} = useAuthContext();
-
+  const { todoList } = useAuthContext();
 
   const handleClick = () => {
-    navigate('/create');
+    navigate("/create");
   };
 
   return (
-    <>
-      <button className="btn btn-active" onClick={handleClick}>Create Todo</button>
+    <div className="mx-10">
+      <h1 className="block  text-3xl text-black my-6"> Task Board</h1>
       {/* {todoList.length ===0 && <h1>You haven't created any todo list yet.</h1>} */}
-      <div className="w-[85%] bg-[url('https://images.pexels.com/photos/998641/pexels-photo-998641.jpeg')] bg-cover bg-center overflow-y-scroll ">
-      <div className="p-4 flex flex-row justify-between items-center">
-        <h1 className="text-white p-10 block  text-3xl"> Task Board</h1> 
-        <div className="available-users self-center">
-          <div className="btn btn-ghost btn-circle avatar">
-            <div className="w-8 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-              />
+      <div className="w-full h-[83vh] bg-[url('https://images.pexels.com/photos/998641/pexels-photo-998641.jpeg')] bg-cover bg-center overflow-y-scroll ">
+        <div className="p-4 flex flex-row justify-between items-center">
+          <button className="btn btn-active" onClick={handleClick}>
+            Create Todo
+          </button>
+          <div className="available-users self-center">
+            <div className="btn btn-ghost btn-circle avatar">
+              <div className="w-8 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                />
+              </div>
             </div>
-          </div>
-          <div className="btn btn-ghost btn-circle avatar">
-            <div className="w-8 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-              />
+            <div className="btn btn-ghost btn-circle avatar">
+              <div className="w-8 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                />
+              </div>
             </div>
-          </div>
-          <div className="btn btn-ghost btn-circle avatar">
-            <div className="w-8 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-              />
+            <div className="btn btn-ghost btn-circle avatar">
+              <div className="w-8 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                />
+              </div>
             </div>
+            <button className="text-3xl text-gray-500 hover:text-slate-950 ">
+              <IoIosAddCircleOutline />
+            </button>{" "}
           </div>
-          <button className="text-3xl text-gray-500 hover:text-slate-950 ">
-            <IoIosAddCircleOutline />
-          </button>{" "}
-     
+        </div>
+
+        <div className="taskboard p-10 pt-0 flex flex-row justify-start gap-3 flex-wrap w-full">
+          {/* card goes here */}
+
+          {todoList.map((todo, idx) => (
+            <TodoList key={idx} todo={todo} />
+          ))}
         </div>
       </div>
-      <div className="taskboard p-10 pt-0 flex flex-row justify-start gap-3 flex-wrap">
-      {/* card goes here */}
-
-      {
-        todoList.map((todo, idx)=>
-          <TodoList key={idx}  todo={todo}/>
-        )
-      }
-      
-      
-      </div>
-      </div>
       {/* <TodoList /> */}
-    </>
+    </div>
   );
 };
 
